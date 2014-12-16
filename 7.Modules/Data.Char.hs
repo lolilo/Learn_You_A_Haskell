27d213eb997de6dd42424d261050ed047f2b6dca
Liana Lo <liana.lo@prezi.com>
2014-12-16 06:41:31 +0100

    Data.List

---
+++ 7.Modules/Data.Char.hs
@@ -0,0 +1,9 @@
+--Caesar cipher
+encode :: Int -> String -> String
+encode shift msg = 
+    let ords = map ord msg
+        shifted = map (+ shift) ords
+    in map chr shifted
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
