36c77e23c676bcfd2e37c615435c33e90ca21a3c
Liana Lo <liana.lo@prezi.com>
2015-01-12 22:19:03 +0100

    randomness

---
+++ 9.Input_and_Output/randomness/random.hs
@@ -0,0 +1,9 @@
+-- random :: (RandomGen g, Random a) => g -> (a, g)
+-- The RandomGen typeclass is for types that can act as sources of randomness.
+-- The Random typeclass is for things that can take on random values
+	-- boolean
+	-- number
+	-- NOT functions!
+
+-- random takes a random generator and 
+-- returns a random values and a new random generator
