32e292356db535844f25b27e456426b57fcd0a58
Liana Lo <liana.lo@prezi.com>
2015-01-24 14:43:46 +0100

    extended rpn calculator

--- 10.Functionally_Solving_Problems/rpn.hs
+++ 10.Functionally_Solving_Problems/rpn.hs
@@ -1,10 +1,15 @@
 import Data.List
 
-solveRPN :: (Num a, Read a) => String -> a
+solveRPN :: String -> Float 
 solveRPN = head . foldl foldingFunction [] . words 
     where 
         foldingFunction (x:y:ys) "*" = (x * y):ys
         foldingFunction (x:y:ys) "+" = (x + y):ys
         foldingFunction (x:y:ys) "-" = (y - x):ys
-        foldingFunction xs numberString = read numberString:xs
-
+        foldingFunction (x:y:ys) "/" = (y / x):ys
+	foldingFunction (x:y:ys) "^" = (y ** x):ys
+	foldingFunction (x:xs) "ln" = log x:xs
+	foldingFunction xs "sum" = [sum xs]
+	foldingFunction xs numberString = read numberString:xs
+	
+-- make this fault tolerant with monads later
